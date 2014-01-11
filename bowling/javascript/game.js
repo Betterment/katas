e8971f14770e753dcf293d3cd2ec01776680d917
Aaron Boushley <aaron@betterment.com>
2014-01-11 10:43:28 -0500

    Added a functional version of bowling scoring.

--- bowling/javascript/game.js
+++ bowling/javascript/game.js
@@ -1,17 +1,94 @@
-function Game() {
-
+function peek(list) {
+    if (list.length > 0) {
+        return list[0];
+    } else {
+        return null;
+    }
+}
+function peek2(list) {
+    if (list.length > 1) {
+        return list[1];
+    } else {
+        return null;
+    }
+}
+function shift(list) {
+    if (list.length > 0) {
+        return list.shift();
+    } else {
+        return null;
+    }
+}
+function push(list, item) {
+    list.push(item);
+    return list;
 }
 
-Game.prototype = {
+function isComplete(frame) {
+    return isBonus(frame) ? frame.length === 3 : frame.length === 2;
+}
 
-    roll: function (numberOfPinsKnockedDown) {
 
-    },
+function deepSum(item) {
+    return item.reduce(function (total, value) {
+        if (Array.isArray(value)) {
+            value = deepSum(value);
+        }
+        return total + value;
+    }, 0);
+}
 
-    score: function () {
-        return 0;
+function buildFrames(rolls, frames) {
+    if (rolls.length === 0 || Game.isComplete(frames)) {
+        return frames;
     }
 
+    rolls = rolls.slice();
+
+    var frame = [shift(rolls)];
+
+    if (!isBonus(frame)) {
+        push(frame, shift(rolls));
+    }
+
+    if (isBonus(frame)) {
+        push(frame, peek(rolls));
+        if (frame.length < 3) {
+            push(frame, peek2(rolls));
+        }
+    }
+
+    frame = frame.filter(function (roll) {
+        return roll !== null;
+    });
+
+    return buildFrames(rolls, push(frames, frame));
+}
+
+var Game = {
+    isBonus: function isBonus(frame) {
+        if (peek(frame) === 10) {
+            return true;
+        }
+
+        if ((peek(frame) + peek2(frame)) === 10) {
+            return true;
+        }
+
+        return false;
+    },
+    framesFromRolls: function framesFromRolls(rolls) {
+        return buildFrames(rolls, []);
+    },
+    score: function (rolls) {
+        return deepSum(framesFromRolls(rolls));
+    },
+    isComplete: function (frames) {
+        return frames.length === 10 && isComplete(frames[9]);
+    }
 };
+
+var isBonus = Game.isBonus;
+var framesFromRolls = Game.framesFromRolls;
 
 module.exports = exports = Game;
--- bowling/javascript/game.spec.js
+++ bowling/javascript/game.spec.js
@@ -3,11 +3,74 @@ var chai = require('chai'),
     should = chai.should();
 
 describe('Game', function () {
-
-    var game;
+    var rolls;
 
     beforeEach(function () {
-        game = new Game();
+        rolls = [];
+    });
+
+    describe('is bonus', function () {
+        it('should not think two gutters are a bonus', function () {
+            Game.isBonus([0,0]).should.be.false;
+        });
+        it('should think a spare is a bonus', function () {
+            Game.isBonus([6,4]).should.be.true;
+        });
+        it('should think a spare is a bonus when equal', function () {
+            Game.isBonus([5,5]).should.be.true;
+        });
+        it('should think a strike is a bonus', function () {
+            Game.isBonus([10]).should.be.true;
+        });
+    });
+
+    describe('frames from rolls', function () {
+        it('should proper frames for a spare followed by all one pin', function () {
+
+            rolls.push(5);
+            rolls.push(5);
+
+            for (var i = 0; i < 18; i++) {
+                rolls.push(1);
+            }
+
+            Game.framesFromRolls(rolls).should.eql([
+                [5,5,1],
+                [1, 1],
+                [1, 1],
+                [1, 1],
+                [1, 1],
+                [1, 1],
+                [1, 1],
+                [1, 1],
+                [1, 1],
+                [1, 1]
+            ]);
+        });
+
+        it('should proper frames for a spare in the last frame', function () {
+
+            for (var i = 0; i < 18; i++) {
+                rolls.push(1);
+            }
+
+            rolls.push(5);
+            rolls.push(5);
+            rolls.push(1);
+
+            Game.framesFromRolls(rolls).should.eql([
+                [1, 1],
+                [1, 1],
+                [1, 1],
+                [1, 1],
+                [1, 1],
+                [1, 1],
+                [1, 1],
+                [1, 1],
+                [1, 1],
+                [5, 5, 1]
+            ]);
+        });
     });
 
     describe('score', function () {
@@ -15,79 +78,79 @@ describe('Game', function () {
         it('should return 0 for a gutter game', function () {
 
             for (var i = 0; i < 20; i++) {
-                game.roll(0);
+                rolls.push(0);
             }
 
-            game.score().should.equal(0);
+            Game.score(rolls).should.equal(0);
         });
 
         it('should return 20 for 1 pin on each roll', function () {
             for (var i = 0; i < 20; i++) {
-                game.roll(1);
+                rolls.push(1);
             }
 
-            game.score().should.equal(20);
+            Game.score(rolls).should.equal(20);
         });
 
         it('should return 29 for a SPARE in the FIRST frame, one pin down in each other roll', function () {
 
-            game.roll(5);
-            game.roll(5);
+            rolls.push(5);
+            rolls.push(5);
 
             for (var i = 0; i < 18; i++) {
-                game.roll(1);
+                rolls.push(1);
             }
 
-            game.score().should.equal(29);
+            Game.score(rolls).should.equal(29);
         });
 
         it('should return 29 for a SPARE in the LAST frame, one pin down in each other roll', function () {
 
             for (var i = 0; i < 18; i++) {
-                game.roll(1);
+                rolls.push(1);
             }
 
-            game.roll(5);
-            game.roll(5);
+            rolls.push(5);
+            rolls.push(5);
 
-            game.roll(1);
+            rolls.push(1);
 
-            game.score().should.equal(29);
+            Game.score(rolls).should.equal(29);
         });
 
         it('should return 30 for a STRIKE in the FIRST frame, one pin down in each other roll', function () {
 
-            game.roll(10);
+            rolls.push(10);
 
             for (var i = 0; i < 18; i++) {
-                game.roll(1);
+                rolls.push(1);
             }
 
-            game.score().should.equal(30);
+            Game.score(rolls).should.equal(30);
         });
 
         it('should return 30 for a STRIKE in the LAST frame, one pin down in each other roll', function () {
 
             for (var i = 0; i < 18; i++) {
-                game.roll(1);
+                rolls.push(1);
             }
 
-            game.roll(10);
+            rolls.push(10);
 
-            game.roll(1);
+            rolls.push(1);
 
-            game.roll(1);
+            rolls.push(1);
 
-            game.score().should.equal(30);
+            Game.score(rolls).should.equal(30);
         });
 
         it('should return 300 for a perfect game', function () {
 
             for (var i = 0; i < 12; i++) {
-                game.roll(10);
+                rolls.push(10);
             }
 
-            game.score().should.equal(300);
+            Game.score(rolls).should.equal(300);
         });
 
     });
